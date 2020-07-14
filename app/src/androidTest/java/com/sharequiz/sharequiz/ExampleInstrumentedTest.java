76d57f655955a5f3693fb037dc4d2b86e84da58b
anandp219 <anandp219@gmail.com>
2020-07-14 13:27:50 +0530

    test file updated

--- app/src/androidTest/java/com/sharequiz/sharequiz/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sharequiz/sharequiz/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package com.sharequiz.sharequiz;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.sharequiz.sharequiz", appContext.getPackageName());
     }
 }
